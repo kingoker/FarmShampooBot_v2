e20a074b6e3abb97a489445f62209cf5a57bce3c
D N <71667614+DaNe-Mi@users.noreply.github.com>
2022-10-11 18:43:52 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -7,7 +7,7 @@ env.read_env()
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = "2045379423:AAF0SFhTxhIEM5pW_46BB1yUFQlraBmakys"
 # BOT_TOKEN = "1936771362:AAHlnu9ACW27yVLcqhX52xxO3dpaD_W5mFI" # Bot toekn Test
-ADMINS = ['1600170280', '3513638']
+ADMINS = ['1600170280', '1019865273', '99940983']
 # ADMINS = ['915850675']# adminlar ro'yxati Test
 IP = "localhost"  # Xosting ip manzili
 PAYMENTS_PROVIDER_TOKEN = '387026696:LIVE:6149b6f02dfdc60f985e6b91'
